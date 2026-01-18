a71304972866f28f00f5292b1a31d5032c6db655
Lameck Irungu <mugolameck@gmail.com>
2026-01-18 19:42:27 +0300

    refactor: rename role-based view functions and update corresponding templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -73,16 +73,16 @@ def is_member(user):
     return user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     """View only accessible to Admin users"""
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'relationship_app/admin.html')
 
 @user_passes_test(is_librarian)
-def librarian_view(request):
+def Librarian(request):
     """View only accessible to Librarian users"""
-    return render(request, 'relationship_app/librarian_view.html')
+    return render(request, 'relationship_app/librarian.html')
 
 @user_passes_test(is_member)
-def member_view(request):
+def Member(request):
     """View only accessible to Member users"""
-    return render(request, 'relationship_app/member_view.html')
+    return render(request, 'relationship_app/member.html')
