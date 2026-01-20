5e785faead521a4417ed330e514ea75a1484a8fc
Lameck Irungu <mugolameck@gmail.com>
2026-01-20 21:36:12 +0300

    fix: update AUTH_USER_MODEL to reference CustomUser in bookshelf app

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -129,4 +129,4 @@ LOGIN_URL = 'relationship_app:login'
 LOGIN_REDIRECT_URL = 'relationship_app:list_books'
 LOGOUT_REDIRECT_URL = 'relationship_app:login'
 
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
