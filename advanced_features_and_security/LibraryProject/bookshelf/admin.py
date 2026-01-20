63bd8d8c831a794a78ce4e11a88bd777b7d139c8
Lameck Irungu <mugolameck@gmail.com>
2026-01-20 21:34:45 +0300

    fix: move code from relationship_app to the appropriate models.py and admin.py in bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Book
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -9,3 +9,15 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    # Add custom fields to the user editing page in Admin
+    fieldsets = list(UserAdmin.fieldsets) + [
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    ]
+    # Add custom fields to the user creation page in Admin
+    add_fieldsets = list(UserAdmin.add_fieldsets) + [
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Author, CustomUser
+from .models import Author
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 admin.site.register(Author, AuthorAdmin)
 
-class CustomUserAdmin(UserAdmin):
-    # Add custom fields to the user editing page in Admin
-    fieldsets = list(UserAdmin.fieldsets) + [
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
-    ]
-    # Add custom fields to the user creation page in Admin
-    add_fieldsets = list(UserAdmin.add_fieldsets) + [
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
-    ]
-
-admin.site.register(CustomUser, CustomUserAdmin)
