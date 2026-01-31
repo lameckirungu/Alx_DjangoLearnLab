bc51d90a36388b732909d5882fa5f96b4d2466c1
Lameck Irungu <mugolameck@gmail.com>
2026-02-01 00:45:33 +0300

    feat: implement BookList view to list all books using Django REST framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
