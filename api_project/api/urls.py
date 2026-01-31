de1b506ce184c01838f436591368f35dc983afb8
Lameck Irungu <mugolameck@gmail.com>
2026-02-01 02:16:11 +0300

    feat: add BookViewSet and update URL routing for enhanced book management

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+# Router URLs for BookViewSet
+router = DefaultRouter()
+router.register('books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Route for the BookList View
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls))
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,15 @@
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Book Viewset for handling CRUD operations
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
