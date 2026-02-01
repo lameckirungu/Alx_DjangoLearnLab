1babdd1845c78de1b3e8f9cb22d19b8c1561c37b
Lameck Irungu <mugolameck@gmail.com>
2026-02-01 03:35:05 +0300

    feat: add token authentication and update settings for REST framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 # Router URLs for BookViewSet
 router = DefaultRouter()
@@ -9,5 +10,6 @@ router.register('books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Route for the BookList View
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -11,5 +13,9 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     Book Viewset for handling CRUD operations
     """
+    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
