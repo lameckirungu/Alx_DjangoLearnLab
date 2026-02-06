aec23ea267bd5abf610b89fd136141a147aea50a
Lameck Irungu <mugolameck@gmail.com>
2026-02-06 15:56:07 +0300

    refactor: enhance docstrings and permissions for book views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,28 +1,56 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BookListView(generics.ListAPIView):
-    """Retrieve all books (GET)"""
+    """
+    Retrieve all books (GET)
+    Allow read-only access to unauthenticated users.
+    """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDetailView(generics.RetrieveAPIView):
-    """Retrieve a single book by ID (GET)"""
+    """
+    Retrieve a single book by ID (GET)
+    Allow read-only access to unauthenticated users.
+    """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookCreateView(generics.CreateAPIView):
-    """Create a new book (POST)"""
+    """
+    Create a new book (POST)
+    REstrict to authenticated users only
+    """
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def perform_create(self, serializer):
+        """Custom behavior during creation"""
+        serializer.save()
+
 class BookUpdateView(generics.UpdateAPIView):
-    """Modify an existing book (PUT/PATCH)"""
+    """
+    Modify an existing book (PUT/PATCH)
+    Restrict to authenticated users only.
+    """
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDeleteView(generics.DestroyAPIView):
-    """Remove a book (DELETE)"""
+    """
+    Remove a book (DELETE)
+    Restrict to authenticated users only.
+    """
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
