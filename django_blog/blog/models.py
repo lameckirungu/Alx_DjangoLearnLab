624286362e3ea71f500a56079dfa2035082e83e9
Lameck Irungu <mugolameck@gmail.com>
2026-02-11 17:56:35 +0300

    feat: add post model, migrations
    fix: setting's INSTALLED_APPS 'blog' app instead of 'django_blog'

---
+++ django_blog/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    """ 
+    Store a blog post authored by a user
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+
