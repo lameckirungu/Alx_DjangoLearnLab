2339ec8273765909dac062857613450d1660810d
Lameck Irungu <mugolameck@gmail.com>
2026-02-13 03:33:13 +0300

    feat: enhance authentication flow and update base template

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -123,13 +123,18 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "blog/static",
 ]
 
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
+LOGIN_REDIRECT_URL = "profile"
