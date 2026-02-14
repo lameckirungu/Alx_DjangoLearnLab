4728badc12545ea9f33c330884d814d640c5fd3d
Lameck Irungu <mugolameck@gmail.com>
2026-02-14 03:53:10 +0300

    feat: add Post model form and CRUD URL patterns

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .views import register, profile
+from .views import PostListView, PostUpdateView, PostCreateView, PostDeleteView, PostDetailView
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", register, name="register"),
     path("profile/", profile, name="profile"),
+
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]   
