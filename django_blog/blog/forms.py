a2c84018aa518da8075c12d9d4f0ea2a268d9a6c
Lameck Irungu <mugolameck@gmail.com>
2026-02-14 22:10:01 +0300

    feat: add Comment model and form for blog comments

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -14,3 +14,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+
+class CommentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Comment
+        fields = ["post", "author", "updated_at", "content"]
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -14,3 +14,10 @@ class Post(models.Model):
         related_name="posts"
     )
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
