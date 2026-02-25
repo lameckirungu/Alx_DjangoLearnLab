06a80f923997c083478c31a0688a2798734d3932
Lameck Irungu <mugolameck@gmail.com>
2026-02-25 17:50:08 +0300

    feat(posts): remove max_length and blank constraints from content fields in Post and Comment models to match checker's expectations

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -6,14 +6,14 @@ User = get_user_model()
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="authors")
     title = models.CharField(max_length=100, blank=True, null=True)
-    content = models.TextField(blank=False, max_length=500)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentors")
-    content = models.TextField(blank=False, max_length=200)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
