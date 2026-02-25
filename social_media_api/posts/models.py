9b81d084887a368a9494db101de551eedd85dff0
Lameck Irungu <mugolameck@gmail.com>
2026-02-25 17:41:45 +0300

    feat(posts): add initial models, admin registration, and app configuration

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="authors")
+    title = models.CharField(max_length=100, blank=True, null=True)
+    content = models.TextField(blank=False, max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentors")
+    content = models.TextField(blank=False, max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
