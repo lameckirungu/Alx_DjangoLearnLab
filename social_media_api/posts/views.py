16bc311d28de097ea960cda49229ec8ce369bb2f
Lameck Irungu <mugolameck@gmail.com>
2026-02-26 17:38:25 +0300

    feat(feed): add FeedView to retrieve posts from followed users

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, BasePermission, SAFE_METHODS
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, BasePermission, SAFE_METHODS, IsAuthenticated
+
 from .models import Post, Comment
+from accounts.models import Follow
 from .serializers import PostSerializer, CommentSerializer
 
 class isOwnerOrReadonly(BasePermission):
@@ -30,3 +32,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        followed_user_ids = Follow.objects.filter(author_id__in=followed_user_ids).select_related("author").order_by("-created_at")
