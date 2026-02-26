9e309e6b9b8e147e51f678f997576ba2af35dbcf
Lameck Irungu <mugolameck@gmail.com>
2026-02-26 17:37:19 +0300

    feat(follow): implement follow and unfollow functionality with validation

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,10 +1,13 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
+
 from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer, UserSerializer
+from .models import Follow
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -50,3 +53,53 @@ class ProfileView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+User = get_user_model()
+
+class FollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        target_user = get_object_or_404(User, id=user_id)
+
+        if target_user == request.user:
+            return Response(
+                {"detail": "You cannot follow yourself"}, 
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        follow_obj, created = Follow.objects.get_or_create(
+            follower=request.user,
+            following=target_user,
+        )
+        if created:
+            return Response(
+                {"detail": f"You are now following {target_user.username}."},
+                status=status.HTTP_200_OK,
+            )
+        
+class UnfollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, user_id):
+        target_user = get_object_or_404(User, id=user_id)
+
+        if target_user == request.user:
+            return Response(
+                {"detail": "You cannot unfollow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    
+    deleted_count, _ = Follow.objects.filter(
+        follower=request.user,
+        following=target_user,
+    ).delete()
+
+    if deleted_count:
+        return Response(
+            {"detail": f"You unfollowed {target_user.username}."},
+            status=status.HTTP_200_OK,
+        )
+    return Response(
+        {"detail": f"You were not following {target_user.username}"},
+        status=status.HTTP_200_OK,
+    )
